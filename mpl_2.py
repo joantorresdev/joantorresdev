31425ed361331e72ca9f0d9dbaaf1e114d2b2706
Juan Torres <66364159+joantorresdev@users.noreply.github.com>
2022-11-17 09:08:00 +0100

    test
    test

---
+++ mpl_2.py
@@ -0,0 +1,14 @@
+
+import matplotlib.pyplot as plt
+
+X = list(range(50))
+Y1 = [x for x in X]
+Y2 = [x**2 for x in X]
+
+plt.subplot2grid((1,2), (0,0), colspan=1, rowspan=1)
+plt.plot(X, Y1)
+
+plt.subplot2grid((1,2), (0,1), colspan=1, rowspan=1)
+plt.plot(X, Y2)
+
+plt.show()
